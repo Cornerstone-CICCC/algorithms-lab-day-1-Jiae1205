48410cab9f8e7108f329e368b3e53625d3f95996
Jiae Park <wldo7237@gmail.com>
2025-05-27 20:24:51 -0700

    ex11

--- ex10.js
+++ ex10.js
@@ -1,7 +1,13 @@
 // Exercise 10: Write a function named 'formatDate' that takes a date string (in the format 'YYYY-MM-DD') and returns a formatted date string (e.g., 'January 1, 2022').
 
 function formatDate(dateStr) {
-  // your code here
+  const date = new Date(dateStr)
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  }
+  return date.toLocaleDateString('en-Us', options)
 }
 
 console.log(formatDate('2022-01-01')) // 'January 1, 2022'
