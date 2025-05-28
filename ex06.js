4e8ee6a899f24900f7d491b0bdb34629dad003f4
Jiae Park <wldo7237@gmail.com>
2025-05-27 20:05:13 -0700

    ex08

--- ex06.js
+++ ex06.js
@@ -1,7 +1,14 @@
 // Exercise 6: Write a function named 'uniqueValues' that takes an array of values and returns a new array with only unique values, removing duplicates.
 
 function uniqueValues(arr) {
-  // your code here
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    let value = arr[i]
+    if (result.includes(value) === false) {
+      result.push(value)
+    }
+  }
+  return result
 }
 
 console.log(uniqueValues([1, 2, 2, 3, 4, 4, 5])) // [1, 2, 3, 4, 5]
